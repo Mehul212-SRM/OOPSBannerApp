49b3e5231f335361d22b01c5e8cc959e89734e4d
Mehul212-SRM <mg1935@srmist.edu.in>
2026-02-22 22:47:22 +0530

    UC4: ASSIGNMENT

---
+++ OOPSBannerApp5.java
@@ -0,0 +1,20 @@
+public class OOPSBannerApp5 {
+
+    public static void main(String[] args) {
+
+        String[] lines = new String[7];
+
+        lines[0] = " *****    *****   ******    ****  ";
+        lines[1] = "*     *  *     *  *     *  *     ";
+        lines[2] = "*     *  *     *  *     *  *     ";
+        lines[3] = "*     *  *     *  ******    ****  ";
+        lines[4] = "*     *  *     *  *             *  ";
+        lines[5] = "*     *  *     *  *             * ";
+        lines[6] = " *****    *****   *         *****  ";
+
+        // for-each loop to print
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
